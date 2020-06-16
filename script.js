33b79886bbf7c637174159ab5b1562cfb22f188a
yusra-khan <yusrakhawar2001@gmail.com>
2020-06-16 07:05:53 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var leftgear = document.getElementById("leftgear");
+window.addEventListener("load", function() {
+
+	let map = document.getElementById("map").contentDocument;
+	let loc = map.getElementById("loc1");
+	console.log(loc);
+	let road = document.getElementById("road");
+	loc.addEventListener('click', () => {
+		road.play();
+	})
+})
+
+let leftgear = document.getElementById("leftgear");
 
 window.addEventListener("scroll", function() {
     leftgear.style.transform = "rotate("+window.pageYOffset+"deg)";
 });
-
-let map = document.getElementById("map").contentDocument;
-let loc = map.getElementById("loc1");
-let road = document.getElementById("road");
-loc.addEventListener('click', () => {
-	road.play();
-})
