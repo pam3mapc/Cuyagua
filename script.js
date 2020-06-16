c266d2f94e2943ad8b2f6664039823a54f6ab51c
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-16 07:28:08 +0500

    Speaker Added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+var leftgear = document.getElementById("leftgear");
+
+window.addEventListener("scroll", function() {
+    leftgear.style.transform = "rotate("+window.pageYOffset+"deg)";
+});
+
 let map = document.getElementById("map").contentDocument;
 let loc = map.getElementById("loc1");
 let road = document.getElementById("road");
 loc.addEventListener('click', () => {
 	road.play();
 })
-
